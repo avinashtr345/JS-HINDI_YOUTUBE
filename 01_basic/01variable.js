11e51fc71aa592d74171df5ee263b4b215693c74
avinashtr345 <134550936+avinashtr345@users.noreply.github.com>
2025-04-03 07:40:00 +0000

    variable

---
+++ 01_basic/01variable.js
@@ -0,0 +1,15 @@
+const accountId=12222
+let accountemail="avi@gmnail.com"
+var accountPass="1234"
+//accountId=2
+console.log(accountId)
+
+accountemail="h@gmai  l.com"
+accountPass="2322"
+console.log(accountemail)
+console.log(accountPass)
+console.table([accountId,acountstate,accountPass,accountemail])
+
+a=2
+console.log(a);
+var acountstate;
