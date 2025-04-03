b3280b89249ef5907f61bd1adb7ada3335d3279a
avinashtr345 <134550936+avinashtr345@users.noreply.github.com>
2025-04-03 07:41:41 +0000

    datatype

---
+++ 01_basic/02datatype.js
@@ -0,0 +1,18 @@
+"use strict"  //treat all js code a newer version
+//alert(3+3)  we are using node js not browser
+console.log(3+2)
+console.log("hitesh")
+let name="avinash"
+let age=12
+let islogged=true
+//number => 2 to ower of 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null => standalone value
+//undefind => declear but not assigned value
+//symbol => to find uuique value in react
+//object
+console.log(typeof age) //number
+console.log(typeof undefined) //undefined
+console.log(typeof null)  //object
