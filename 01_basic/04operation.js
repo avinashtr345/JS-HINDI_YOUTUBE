a4c34e697246815e1f6c52aef5f6df1d16963229
avinashtr345 <134550936+avinashtr345@users.noreply.github.com>
2025-04-04 05:41:22 +0000

    operaqtion

---
+++ 01_basic/04operation.js
@@ -0,0 +1,16 @@
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+"2");//12
+console.log("1"+2+2); //122
+//agar string 1st hain to sbko string treate hoga
+console.log(1+2+"2");  //32
+//agar string last hain pahle add hoga
+console.log(true) //true
+console.log(+true) //1
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
