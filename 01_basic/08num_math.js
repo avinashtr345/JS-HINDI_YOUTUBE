517a19bc77397721e1b529510d5810a19bd71cad
avinashtr345 <134550936+avinashtr345@users.noreply.github.com>
2025-04-04 12:39:51 +0000

    math&num

---
+++ 01_basic/08num_math.js
@@ -0,0 +1,23 @@
+const score=200;
+console.log(score) //100
+const a=new Number(100)
+console.log(a) //[Number:100]
+console.log(a.toString().length)
+console.log(typeof(a))
+
+console.log(a.toFixed(2))
+console.log(a.toFixed(1))
+
+const x=10000000
+console.log(x.toLocaleString('en-IN')) //comma aayega
+
+
+
+console.log(Math.abs(-4))
+console.log(Math.round(4.522))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.2))
+console.log(Math.min(2,3,4,5))
+console.log(Math.max(2,3,4,5))
+console.log(Math.random()) //0 to 1 ke beech me value aayega
+console.log(Math.random()*10) 
