4b303e23684639c688f74589708420ef84e3877f
avinashtr345 <134550936+avinashtr345@users.noreply.github.com>
2025-04-04 05:28:22 +0000

    typeconversion

---
+++ 01_basic/03conversiontype.js
@@ -0,0 +1,28 @@
+let score='22';
+let s='12ab'
+console.log(typeof(score)) //numbwe
+console.log(typeof(score))
+
+let valuein=Number(score) //string conver into number
+console.log(typeof(valuein))
+
+
+let a=Number(s)
+console.log(typeof(a))
+console.log(a)
+
+let x=null;
+let y=Number(x)
+console.log(typeof(y))
+console.log(y);
+
+//"33"=>33
+//"33abc"=>NaN
+//true =>1 false =>0
+
+let islogin=""
+
+let booleanislog=Boolean(islogin)
+console.log(booleanislog)
+//""==>false
+//"avinash" =>true;
