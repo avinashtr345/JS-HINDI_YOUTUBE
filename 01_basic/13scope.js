9941d1eeb25c3e21aed0c5a97b58dfb403189c0c
avinashtr345 <134550936+avinashtr345@users.noreply.github.com>
2025-04-07 12:10:08 +0000

    scope

---
+++ 01_basic/13scope.js
@@ -0,0 +1,40 @@
+//var c=300
+let a=300
+if(true){
+    let a=10;
+    const b=20;
+    //var c=30;
+    console.log(a)
+    
+}
+//console.log(a)
+//console.log(b)
+console.log(a)
+
+//hositing
+
+function one(){
+    const username="avi"
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    //console.log(website)
+    two()
+}
+one()
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num+2;
+}//es case me uper access kr skte h
+//console.log(addone(5))
+
+
+console.log(addtwo(5))
+const addtwo=function(num){
+    return num+2;
+}  //ess case me upper acces nhi kr skte
+//console.log(addtwo(5))
