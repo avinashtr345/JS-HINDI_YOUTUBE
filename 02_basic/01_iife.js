4d829a1b6966106d26e52a2131f27aadb45d1ce3
avinashtr345 <134550936+avinashtr345@users.noreply.github.com>
2025-04-08 09:50:53 +0000

    IIFE

---
+++ 02_basic/01_iife.js
@@ -0,0 +1,13 @@
+//Immediately invoked function expressions(IIFE)
+(function chai(){
+    //named IIFE
+    console.log(`DB CONNECTED`)
+})();
+
+( (hites)=>{
+    console.log(`DB connected2`)
+})('hites');
+
+
+//two IIFE ek sath likhna yhain t semicollon laga do
+//IMPORTTANT IN INTERVIEW
